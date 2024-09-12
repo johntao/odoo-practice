0072faa5c9c062df52b62de33fb8a23422daf6da
John Tao <80338582+johntao@users.noreply.github.com>
2024-09-12 16:37:38 +0800

    feat: Ch6 introduce custom views
    refactor: remove unused code

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -12,12 +12,6 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    # def __init__(self, dt1=None, dt2=None,
-    #              years=0, months=0, days=0, leapdays=0, weeks=0,
-    #              hours=0, minutes=0, seconds=0, microseconds=0,
-    #              year=None, month=None, day=None, weekday=None,
-    #              yearday=None, nlyearday=None,
-    #              hour=None, minute=None, second=None, microsecond=None):
     def _get_default_date(self):
         return fields.Date.context_today(self) + relativedelta(months=3)
     date_availability = fields.Date(copy=False, default=_get_default_date)
@@ -29,6 +23,9 @@ class EstateProperty(models.Model):
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    garden_orientation = fields.Selection(string='Type',
+            selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+            help="garden_orientation")
     active = fields.Boolean(default=True)
     state = fields.Selection([
         ('new', 'New'),
@@ -37,13 +34,3 @@ class EstateProperty(models.Model):
         ('sold', 'Sold'),
         ('canceled', 'Canceled')
         ], string='Status', default='new', copy=False, required=True)
-    garden_orientation = fields.Selection(string='Type',
-            selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-            help="garden_orientation")
-    # number_of_months = fields.Integer(required=True)
-    # active = fields.Boolean(default=True)
-    # sequence = fields.Integer(default=10)
-# Field	Type
-    # _sql_constraints = [
-    #     ('check_number_of_months', 'CHECK(number_of_months >= 0)', 'The number of month can\'t be negative.'),
-    # ]
